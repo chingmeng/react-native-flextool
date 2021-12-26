ef558d883d0876d561d01e717f4827775405ad5d
Meng <chingmeng.hong@hexainitiative.com>
2021-12-26 08:24:46 +0800

    Enable FLEX during debug only

--- ios/reactNativeFlex/RNFlipboardFlex.m
+++ ios/reactNativeFlex/RNFlipboardFlex.m
@@ -7,7 +7,10 @@
 //
 
 #import "RNFlipboardFlex.h"
+#ifdef DEBUG
 #import "FLEXManager.h"
+#endif
+
 
 @implementation RNFlipboardFlex
 
@@ -16,23 +19,29 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(showExplorer)
 {
+    #ifdef DEBUG
     dispatch_async(dispatch_get_main_queue(), ^{
         [[FLEXManager sharedManager] showExplorer];
     });
+    #endif
 }
 
 RCT_EXPORT_METHOD(hideExplorer)
 {
+    #ifdef DEBUG
     dispatch_async(dispatch_get_main_queue(), ^{
         [[FLEXManager sharedManager] hideExplorer];
     });
+    #endif
 }
 
 RCT_EXPORT_METHOD(toggleExplorer)
 {
+    #ifdef DEBUG
     dispatch_async(dispatch_get_main_queue(), ^{
         [[FLEXManager sharedManager] toggleExplorer];
     });
+    #endif
 }
 
 @end
